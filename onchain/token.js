2022d697adc30e8993d7985154e637155b213b0c
PandaKewt <chuminhtuan30306@gmail.com>
2022-08-28 12:07:31 +0700

    Update 2hr #8

--- onchain/token.js
+++ onchain/token.js
@@ -4,11 +4,13 @@ const idlFactory = require('./idlFactory');
 
 const Wallet = async () => {
     const canisterId = 'rrkah-fqaaa-aaaaa-aaaaq-cai';
-    //rgwjw-3aaaa-aaaal-qbcma-cai
+    // Local rrkah-fqaaa-aaaaa-aaaaq-cai
+    // Mainnet rgwjw-3aaaa-aaaal-qbcma-cai
     const identity = initIdentity();
 
     const agent = new HttpAgent({identity, fetch, host: "http://127.0.0.1:8000" });
-    //https://mainnet.dfinity.network
+    //Local http://127.0.0.1:8000
+    //Mainnet https://mainnet.dfinity.network
     await agent.fetchRootKey();
 
     return Actor.createActor(idlFactory, {
